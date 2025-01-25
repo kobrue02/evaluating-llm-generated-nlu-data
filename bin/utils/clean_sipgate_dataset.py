5639d80c4e3ccd6f7c0a396ad6cdc5bdf979ff8c
Konrad Brüggemann <brueggemann4@uni-potsdam.de>
2025-01-25 11:32:44 +0100

    data processing

--- bin/utils/clean_sipgate_dataset.py
+++ bin/utils/clean_sipgate_dataset.py
@@ -5,6 +5,7 @@ def clean_sipgate_dataset(df: pd.DataFrame) -> pd.DataFrame:
     """
     Clean the sipgate dataset by removing duplicates, intents with too few samples and samples with low occurrences.
     """
+
     # deduplicate text column
     df = df.drop_duplicates(subset="text")
 
@@ -12,9 +13,16 @@ def clean_sipgate_dataset(df: pd.DataFrame) -> pd.DataFrame:
     df = df.groupby("intent").filter(lambda x: len(x) > 100 and len(x) < 500)
 
     # keep only intents that don't start with "_"
-    df = df[~df.index.str.startswith("_")]
+    df = df[~df.intent.str.startswith("_")]
 
     # for each intent, keep the 100 samples with highest value in occurrences column
     df = df.groupby("intent").apply(lambda x: x.nlargest(100, "occurrences"))
 
+    df.reset_index(drop=True, inplace=True)
     return df
+
+if __name__ == "__main__":
+    from bin.utils.read_datasets import read_sipgate_dataset
+    df = read_sipgate_dataset()
+    df = clean_sipgate_dataset(df)
+    print(df.head())
