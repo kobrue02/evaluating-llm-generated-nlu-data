8b5d9360d847d16e8cf9e7fb91ed8f08eabe1856
Konrad Brüggemann <brueggemann4@uni-potsdam.de>
2025-01-25 11:24:11 +0100

    data processing

---
+++ bin/utils/clean_sipgate_dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def clean_sipgate_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean the sipgate dataset by removing duplicates, intents with too few samples and samples with low occurrences.
+    """
+    # deduplicate text column
+    df = df.drop_duplicates(subset="text")
+    df.set_index("intent", inplace=True)
+
+    # keep only intents with more than 20 and less than 1000 samples
+    df = df.groupby("intent").filter(lambda x: len(x) > 100 and len(x) < 500)
+
+    # keep only intents that don't start with "_"
+    df = df[~df.index.str.startswith("_")]
+
+    # for each intent, keep the 100 samples with highest value in occurrences column
+    df = df.groupby("intent").apply(lambda x: x.nlargest(100, "occurrences"))
+
+    return df
--- bin/utils/read_datasets.py
+++ bin/utils/read_datasets.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 
-def read_sipgate_dataset():
-    full_df = pd.read_csv("data/sipgate_data.csv")
-    dataset = full_df[["text", "phraseIntent"]].copy()
+def read_sipgate_dataset() -> pd.DataFrame:
+    """
+    Read the sipgate dataset and return a DataFrame with the specified columns.
+    If columns is None, return the full DataFrame.
+    """
+    dataset = pd.read_csv("data/sipgate_data.csv")
     dataset.rename(columns={"phraseIntent": "intent"}, inplace=True)
+    dataset = dataset[["text", "intent", "occurrences", "phraseEntTypes", "annotations"]]
     return dataset
 
 
